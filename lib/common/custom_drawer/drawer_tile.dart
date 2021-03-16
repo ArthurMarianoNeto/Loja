e21ecaeeb76c1ca79404e9fbe5a3570ca9eca721
ArthurMarianoNeto <thiesto@gmail.com>
2021-03-16 09:20:58 -0300

    #16 Navegando via provider

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:loja/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
 
   const DrawerTile({this.iconData, this.title, this.page});
-
   final IconData iconData;
   final String title;
   final int page;
 
   @override
   Widget build(BuildContext context) {
+    final int curPage = context.watch<PageManager>().page;
+
     return InkWell(
       onTap: (){
-        debugPrint('toquei $page');
+        context.read<PageManager>().setPage(page);
       },
       child: SizedBox(
         height: 60,
@@ -23,14 +26,14 @@ class DrawerTile extends StatelessWidget {
               child: Icon(
                 iconData,
                 size: 32,
-                color: Colors.grey[700],
+                color: curPage == page ? Colors.red : Colors.grey[700],
               ),
             ),
             Text(
               title,
               style: TextStyle(
                   fontSize: 16,
-                  color: Colors.grey[700]
+                  color: curPage == page ? Colors.red : Colors.grey[700]
               ),
             )
           ],
