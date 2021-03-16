2989568468bdcdf341a8740467dcf5e817d45ae0
ArthurMarianoNeto <thiesto@gmail.com>
2021-03-16 08:26:59 -0300

    #14 Criando Drawer customizado

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja/screens/base/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja do Daniel',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
