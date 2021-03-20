2a84ae9d2a98d7e7e2f622690af85ddabcce8d93
ArthurMarianoNeto <thiesto@gmail.com>
2021-03-20 15:30:39 -0300

    #35 - Criando campo de pesquisa, pesquisado produto pelo nome, editando a pesquisa.

--- lib/screens/products/products_screen.dart
+++ lib/screens/products/products_screen.dart
@@ -3,7 +3,6 @@ import 'package:loja/common/custom_drawer/custom_drawer.dart';
 import 'package:loja/models/product_manager.dart';
 import 'package:loja/screens/products/components/product_list_tile.dart';
 import 'package:provider/provider.dart';
-
 import 'components/search_dialog.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -12,24 +11,69 @@ class ProductsScreen extends StatelessWidget {
     return Scaffold(
       drawer: CustomDrawer(),
       appBar: AppBar(
-        title: const Text('Produtos'),
+        title: Consumer<ProductManager>(
+          builder: (_, productManager, __){
+            if(productManager.search.isEmpty){
+              return const Text('Produtos');
+            } else {
+              return LayoutBuilder(
+                builder: (_, constraints){
+                  return GestureDetector(
+                    onTap: () async {
+                      final search = await showDialog<String>(context: context,
+                          builder: (_) => SearchDialog(productManager.search));
+                      if(search != null){
+                        productManager.search = search;
+                      }
+                    },
+                    child: Container(
+                        width: constraints.biggest.width,
+                        child: Text(
+                          productManager.search,
+                          textAlign: TextAlign.center,
+                        )
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
         centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){
-              showDialog(context: context, builder: (_) => SearchDialog());
+        actions: <Widget>[
+          Consumer<ProductManager>(
+            builder: (_, productManager, __){
+              if(productManager.search.isEmpty){
+                return IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () async {
+                    final search = await showDialog<String>(context: context,
+                        builder: (_) => SearchDialog(productManager.search));
+                    if(search != null){
+                      productManager.search = search;
+                    }
+                  },
+                );
+              } else {
+                return IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () async {
+                    productManager.search = '';
+                  },
+                );
+              }
             },
           )
         ],
       ),
       body: Consumer<ProductManager>(
         builder: (_, productManager, __){
+          final filteredProducts = productManager.filteredProducts;
           return ListView.builder(
               padding: const EdgeInsets.all(4),
-              itemCount: productManager.allProducts.length,
+              itemCount: filteredProducts.length,
               itemBuilder: (_, index){
-                return ProductListTile(productManager.allProducts[index]);
+                return ProductListTile(filteredProducts[index]);
               }
           );
         },
