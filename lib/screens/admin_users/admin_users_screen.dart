07ddd60795b0612adad40aec4d649bd1dcebc113
ArthurMarianoNeto <thiesto@gmail.com>
2021-04-22 15:11:15 -0300

    62 - Buscando usuário no firebase

--- lib/screens/admin_users/admin_users_screen.dart
+++ lib/screens/admin_users/admin_users_screen.dart
@@ -1,4 +1,5 @@
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
+import 'package:loja/common/custom_drawer/custom_drawer.dart';
 import 'package:loja/models/admin_users_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +8,7 @@ class AdminUsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
       appBar: AppBar(
         title: const Text('Usuários'),
         centerTitle: true,
