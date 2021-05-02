fc559e898de8fcfe613f1e05438d4ce30da7593e
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-02 19:47:23 -0300

    89 - Revovento imagens da tela principal

--- lib/screens/home/components/item_tile.dart
+++ lib/screens/home/components/item_tile.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:loja/models/home_manager.dart';
 import 'package:loja/models/product_manager.dart';
+import 'package:loja/models/section.dart';
 import 'package:loja/models/section_item.dart';
 import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
@@ -13,6 +15,9 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final homeManager = context.watch<HomeManager>();
+
     return GestureDetector(
       onTap: (){
         if(item.product != null){
@@ -23,6 +28,26 @@ class ItemTile extends StatelessWidget {
           }
         }
       },
+      onLongPress: homeManager.editing ? (){
+        showDialog(
+            context: context,
+          builder: (_) {
+              return AlertDialog(
+                title: const Text('Editar Item'),
+                actions: [
+                  FlatButton(
+                      onPressed: (){
+                         context.read<Section>().removeItem(item);
+                         Navigator.of(context).pop();
+                      },
+                    textColor: Colors.red,
+                      child: const Text('Excluir'),
+                  )
+                ],
+              );
+          }
+        );
+    } : null,
       child: AspectRatio(
         aspectRatio: 1,
         child: item.image is String
