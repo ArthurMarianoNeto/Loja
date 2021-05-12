bdf90df9fdc6abce99c61819967e59aad02d7ae6
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-12 17:04:05 -0300

    111 - Verificando Estoque e decremento, evitando que dois clientes comprem 1 unidade em estoque

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loja/models/cart_manager.dart';
-
+import 'package:loja/models/product.dart';
 
 class CheckoutManager extends ChangeNotifier {
 
   CartManager cartManager;
+
   final Firestore firestore = Firestore.instance;
 
   // ignore: use_setters_to_change_properties
@@ -13,15 +14,13 @@ class CheckoutManager extends ChangeNotifier {
     this.cartManager = cartManager;
   }
 
-  void checkout() {
-    _decrementStock();
-    _getOrderId().then((value) => print(value));
-  }
+  Future<void> checkout() async {
+    try {
+      await _decrementStock();
+    } catch (e){
+      debugPrint(e.toString());
+    }
 
-  void _decrementStock(){
-    // 1. Ler todos os estoques 3xM
-    // 2. Decremento localmente os estoques 2xM
-    // 3. Salvar os estoques no firebase 2xM
     _getOrderId().then((value) => print(value));
   }
 
@@ -29,7 +28,7 @@ class CheckoutManager extends ChangeNotifier {
     final ref = firestore.document('aux/ordercounter');
 
     try {
-      final result = await firestore.runTransaction((tx) async { // tx = transaction
+      final result = await firestore.runTransaction((tx) async {
         final doc = await tx.get(ref);
         final orderId = doc.data['current'] as int;
         await tx.update(ref, {'current': orderId + 1});
@@ -39,8 +38,52 @@ class CheckoutManager extends ChangeNotifier {
     } catch (e){
       debugPrint(e.toString());
       return Future.error('Falha ao gerar número do pedido');
-      // tentará gerar 5 vezes o numero de pedido, depois disto "erro";
     }
+  }
+
+  Future<void> _decrementStock(){ // verificando estoque
+    // 1. Ler todos os estoques 3xM
+    // 2. Decremento localmente os estoques 2xM
+    // 3. Salvar os estoques no firebase 2xM
+
+    return firestore.runTransaction((tx) async {
+      final List<Product> productsToUpdate = [];
+      final List<Product> productsWithoutStock = [];
+
+      for(final cartProduct in cartManager.items){
+        Product product;
+
+        if(productsToUpdate.any((p) => p.id == cartProduct.productId)){
+          product = productsToUpdate.firstWhere(
+                  (p) => p.id == cartProduct.productId);
+        } else {
+          final doc = await tx.get(
+              firestore.document('products/${cartProduct.productId}')
+          );
+          product = Product.fromDocument(doc);
+        }
+
+        cartProduct.product = product;
+
+        final size = product.findSize(cartProduct.size);
+        if(size.stock - cartProduct.quantity < 0){
+          productsWithoutStock.add(product);
+        } else {
+          size.stock -= cartProduct.quantity;
+          productsToUpdate.add(product);
+        }
+      }
+
+      if(productsWithoutStock.isNotEmpty){
+        return Future.error(
+            '${productsWithoutStock.length} produtos sem estoque');
+      }
+
+      for(final product in productsToUpdate){
+        tx.update(firestore.document('products/${product.id}'),
+            {'sizes': product.exportSizeList()});
+      }
+    });
   }
 
 }
