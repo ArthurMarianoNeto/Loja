38ac730edabc60b73f9dcddf09d3ffccea4d0de2
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-12 16:37:06 -0300

    110 - Gerando número unico incremental de Pedido

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loja/models/cart_manager.dart';
 
@@ -5,6 +6,7 @@ import 'package:loja/models/cart_manager.dart';
 class CheckoutManager extends ChangeNotifier {
 
   CartManager cartManager;
+  final Firestore firestore = Firestore.instance;
 
   // ignore: use_setters_to_change_properties
   void updateCart(CartManager cartManager){
@@ -13,12 +15,32 @@ class CheckoutManager extends ChangeNotifier {
 
   void checkout() {
     _decrementStock();
+    _getOrderId().then((value) => print(value));
   }
 
   void _decrementStock(){
     // 1. Ler todos os estoques 3xM
     // 2. Decremento localmente os estoques 2xM
     // 3. Salvar os estoques no firebase 2xM
+    _getOrderId().then((value) => print(value));
+  }
+
+  Future<int> _getOrderId() async {
+    final ref = firestore.document('aux/ordercounter');
+
+    try {
+      final result = await firestore.runTransaction((tx) async { // tx = transaction
+        final doc = await tx.get(ref);
+        final orderId = doc.data['current'] as int;
+        await tx.update(ref, {'current': orderId + 1});
+        return {'orderId': orderId};
+      });
+      return result['orderId'] as int;
+    } catch (e){
+      debugPrint(e.toString());
+      return Future.error('Falha ao gerar número do pedido');
+      // tentará gerar 5 vezes o numero de pedido, depois disto "erro";
+    }
   }
 
 }
