2a6d9d9f58783dfd5b4b5435731a93576421d5a7
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-19 10:58:58 -0300

    130 - Apresentando opção de delete de produto e setando produto como "falso"

--- lib/models/product_manager.dart
+++ lib/models/product_manager.dart
@@ -34,7 +34,7 @@ class ProductManager extends ChangeNotifier {
 
   Future<void> _loadAllProducts() async {
     final QuerySnapshot snapProducts =
-        await firestore.collection('products').getDocuments();
+        await firestore.collection('products').where('deleted', isEqualTo: false).getDocuments();
     allProducts =
         snapProducts.documents.map((d) => Product.fromDocument(d)).toList();
     notifyListeners();
@@ -51,6 +51,11 @@ class ProductManager extends ChangeNotifier {
   void update(Product product) {
     allProducts.removeWhere((p) => p.id == product.id);
     allProducts.add(product);
+    notifyListeners();
+  }
+  void delete(Product product){
+    product.delete();
+    allProducts.removeWhere((p) => p.id == product.id);
     notifyListeners();
   }
 }
