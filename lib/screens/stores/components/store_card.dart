2278ba7ff4685f28618b4c2055d45f07cb99dc1a
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-25 07:54:49 -0300

    145 - Exibindo a Loja e chamando os aplicativos com coordenadas de google maps

--- lib/screens/stores/components/store_card.dart
+++ lib/screens/stores/components/store_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja/common/custom_icon_button.dart';
 import 'package:loja/models/store.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:map_launcher/map_launcher.dart';
 
 class StoreCard extends StatelessWidget {
 
@@ -26,23 +27,64 @@ class StoreCard extends StatelessWidget {
       }
     }
 
+    void showError(){
+      Scaffold.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Esta função não está disponível neste dispositivo'),
+            backgroundColor: Colors.red,
+          )
+      );
+    }
+
     Future<void> openPhone() async {
       if(await canLaunch('tel:${store.cleanPhone}')){
         launch('tel:${store.cleanPhone}');
       } else {
-        Scaffold.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Esta função não está disponível neste dispositivo'),
-              backgroundColor: Colors.red,
-            )
+        showError();
+      }
+    }
+
+    Future<void > openMap() async {
+      try {
+        final availableMaps = await MapLauncher.installedMaps;
+
+        showModalBottomSheet(
+            context: context,
+            builder: (_) {
+              return SafeArea(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    for(final map in availableMaps)
+                      ListTile(
+                        onTap: (){
+                          map.showMarker(
+                            coords: Coords(store.address.lat, store.address.long),
+                            title: store.name,
+                            description: store.addressText,
+                          );
+                          Navigator.of(context).pop();
+                        },
+                        title: Text(map.mapName),
+                        leading: Image(
+                          image: map.icon,
+                          width: 30,
+                          height: 30,
+                        ),
+                      )
+                  ],
+                ),
+              );
+            }
         );
+      } catch (e){
+        showError();
       }
     }
 
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
       clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: <Widget>[
           Container(
@@ -112,11 +154,9 @@ class StoreCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     CustomIconButton(
-                      iconData: Icons.map,
+                      iconData: Icons.location_on,
                       color: primaryColor,
-                      onTap: (){
-
-                      },
+                      onTap: openMap,
                     ),
                     CustomIconButton(
                       iconData: Icons.phone,
@@ -127,6 +167,7 @@ class StoreCard extends StatelessWidget {
                       iconData: Icons.chat,
                       color: primaryColor,
                       onTap: (){
+                        //TODO WHATSUP
 
                       },
                     ),
