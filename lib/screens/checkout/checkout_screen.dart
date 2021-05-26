0f934a9b1038cd57656a8a7abe1f4eadf372720d
ArthurMarianoNeto <thiesto@gmail.com>
2021-05-26 09:28:32 -0300

    146 - Criando o shape do cartão de crédito animado

--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -3,6 +3,7 @@ import 'package:loja/common/price_card.dart';
 import 'package:loja/models/cart_manager.dart';
 import 'package:loja/models/checkout_manager.dart';
 import 'package:loja/models/page_manager.dart';
+import 'package:loja/screens/checkout/components/credit_card_widget.dart';
 import 'package:provider/provider.dart';
 
 class CheckoutScreen extends StatelessWidget {
@@ -47,6 +48,7 @@ class CheckoutScreen extends StatelessWidget {
             }
             return ListView(
               children: <Widget>[
+                CreditCardWidget(),
                 PriceCard(
                   buttonText: 'Finalizar Pedido',
                   onPressed: (){
