1d136dd1dfe66b9d6ee247e5e068eb95a71ae17e
ArthurMarianoNeto <thiesto@gmail.com>
2021-06-24 16:06:33 -0300

    179 - Exibindo notificação Interna com o APP ativo, fechado e em segundo plano.

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loja/common/custom_drawer/custom_drawer.dart';
@@ -31,6 +34,47 @@ class _BaseScreenState extends State<BaseScreen> {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp
     ]);
+
+    configFCM();
+  }
+
+  void configFCM(){
+    final fcm = FirebaseMessaging();
+
+    if(Platform.isIOS){
+      fcm.requestNotificationPermissions(
+          const IosNotificationSettings(provisional: true)
+      );
+    }
+
+    fcm.configure(
+        onLaunch: (Map<String, dynamic> message) async {
+          print('onLaunch $message');
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print('onResume $message');
+        },
+        onMessage: (Map<String, dynamic> message) async {
+          showNotification(
+            message['notification']['title'] as String,
+            message['notification']['body'] as String,
+          );
+        }
+    );
+  }
+
+  void showNotification(String title, String message){
+    Flushbar(
+      title: title,
+      message: message,
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.GROUNDED,
+      isDismissible: true,
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: const Duration(seconds: 5),
+      icon: Icon(Icons.shopping_cart, color: Colors.white,),
+    ).show(context);
+
   }
 
   @override
